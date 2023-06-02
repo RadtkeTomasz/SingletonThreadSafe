d2bd634e9e409b9a471faf98869fe131cbd58d73
TomaszR <tomeas23@email.com>
2023-06-02 15:29:55 +0200

    Add project files.

---
+++ SingletonThreadSafe/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Singleton
+{
+    class SingletonExample
+    {
+        private SingletonExample() { }
+
+        private static SingletonExample _instance;
+
+        private static readonly object _lock = new object();
+
+        public static SingletonExample GetInstance(string value)
+        {
+
+            if (_instance == null)
+            {
+                lock (_lock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new SingletonExample();
+                    }
+                }
+            }
+            return _instance;
+        }
+    }
+}
